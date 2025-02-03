3409a49bc2ebb363f509f31cd75a017b61c2570c
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2025-02-03 15:57:57 +0100

    Update plugins and transparent background for rose-pine

--- lua/config/editor.lua
+++ lua/config/editor.lua
@@ -6,6 +6,18 @@ require('catppuccin').setup({
   },
   transparent_background = true
 })
+
+require('rose-pine').setup({
+  extend_background_behind_borders = true,
+  enable = {
+    terminal = true,
+    italics = true,
+    underline = true
+  },
+  styles = {
+    transparency = true,
+  }
+})
 -- Set lualine as statusline
 -- See `:help lualine.txt`
 require('lualine').setup ({
