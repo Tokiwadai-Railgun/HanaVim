bee4b033d09d8efda0d844768f9df60ba11b7b14
Tokiwadai-Railgun <arthur.scheidbach@proton.me>
2026-03-12 10:53:11 +0100

    Added a plugin for git permalink

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -119,5 +119,14 @@ return {
 				dependencies = {
 					"nvim-treesitter/nvim-treesitter",
 				}
-			}
+		},
+		{
+				"linrongbin16/gitlinker.nvim",
+				cmd = "GitLink",
+				opts = {},
+				keys = {
+						{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+						{ "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+				},
 		}
+}
